936bd75cb2c61f3e1ccc08aca0443f0a598e951a
Atsushi Hosaka <hosaka2917@gmail.com>
2024-09-29 15:41:43 +0900

    Initial Commit

---
+++ github_actions_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github_actions_test
+//
+//  Created by 保坂篤志 on 2024/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ github_actions_testTests/github_actions_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  github_actions_testTests.swift
+//  github_actions_testTests
+//
+//  Created by 保坂篤志 on 2024/09/29.
+//
+
+import Testing
+@testable import github_actions_test
+
+struct github_actions_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
